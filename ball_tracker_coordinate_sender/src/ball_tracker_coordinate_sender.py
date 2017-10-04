7a9e677d2797dad992b0fc7c1bbb0005e0f3ead2
carebare47 <tom_q@hotmail.co.uk>
2017-10-04 19:30:04 +0100

    added startCamera function, with resolution and FPS set

--- ball_tracker_coordinate_sender/src/ball_tracker_coordinate_sender.py
+++ ball_tracker_coordinate_sender/src/ball_tracker_coordinate_sender.py
@@ -2,6 +2,7 @@
 # license removed for brevity
 import rospy
 from geometry_msgs.msg import Point
+import cv2
 
 def coordinate_sender():
 	pub = rospy.Publisher('coordinate_send_topic', Point, queue_size=10)
@@ -16,8 +17,23 @@ def coordinate_sender():
 		rate.sleep()
 
 
+def startCamera():
+	rospy.loginfo("Create video capture pointer")
+	cap = cv2.VideoCapture(1)
+
+	rospy.loginfo("Setting resolution to 320x240")
+	cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 320)
+	cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 240)
+
+	rospy.loginfo("Setting frame rate")
+	cap.set(cv2.cv.CV_CAP_PROP_FPS, 250)
+
+	
+
+
 if __name__ == '__main__':
-    try:
-        coordinate_sender()
-    except rospy.ROSInterruptException:
-        pass
+	startVideo()
+	try:
+		coordinate_sender()
+	except rospy.ROSInterruptException:
+        	pass
