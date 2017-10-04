607258a4e89fcb0b730e41cac68e63c9a7ebaf6e
carebare47 <tom_q@hotmail.co.uk>
2017-10-04 20:42:54 +0100

    added motor controll functionallity

--- coordinate_reciever_arduino/coordinate_reciever_arduino.ino
+++ coordinate_reciever_arduino/coordinate_reciever_arduino.ino
@@ -1,3 +1,5 @@
+#include <PID_v1.h>
+
 /*
  * Arduino rosserial coordinate reciever
  * Written by Tom Queen with Arduino 1.0.6 and ROS Indigo
@@ -11,6 +13,23 @@
 
 //Create node handle
 ros::NodeHandle nh;
+
+
+#define motor_A_1 2
+#define motor_A_2 4
+#define motor_A_EN 3
+
+#define motor_B_1 7
+#define motor_B_2 6
+#define motor_B_EN 5
+
+#define motor_C_1 8
+#define motor_C_2 9
+#define motor_C_EN 10
+
+#define motor_D_1 13
+#define motor_D_2 12
+#define motor_D_EN 11
 
 
 //Variables to hold coordinates for end effector
@@ -27,6 +46,7 @@ void messageCb( const geometry_msgs::Point& data){
 ros::Subscriber<geometry_msgs::Point> coordinate_subscriber("coordinate_sender", &messageCb);
 
 void setup () {
+  motorSetup();
   
   //Initialize node handler
   nh.initNode();
@@ -46,5 +66,85 @@ void loop () {
 
 void motorController(int x, int y){
   //Do something with the recieved coordinates
-}
   
+  //assuming point x will be between 0 and 320 & point y between 0 and 240
+  if (x < 160){
+      direction_left();  
+}
+
+void motorSetup(){
+ pinMode(motor_A_1, OUTPUT);
+ pinMode(motor_A_2, OUTPUT);
+ pinMode(motor_A_EN, OUTPUT);
+ 
+ pinMode(motor_B_1, OUTPUT);
+ pinMode(motor_B_2, OUTPUT);
+ pinMode(motor_B_EN, OUTPUT);
+ 
+ pinMode(motor_C_1, OUTPUT);
+ pinMode(motor_C_2, OUTPUT);
+ pinMode(motor_C_EN, OUTPUT);
+ 
+ pinMode(motor_D_1, OUTPUT);
+ pinMode(motor_D_2, OUTPUT);
+ pinMode(motor_D_EN, OUTPUT);
+ 
+}
+
+
+void set_direction_left(){
+  A_forwards();
+  C_forwards();
+  
+  B_reverse();
+  D_reverse();
+}
+
+
+
+
+void A_forwards(){
+  digitalWrite(motor_A_1, HIGH);
+  digitalWrite(motor_A_2, LOW);
+}
+
+void A_reverse(){
+  digitalWrite(motor_A_2, HIGH);
+  digitalWrite(motor_A_1, LOW);
+}
+
+
+void B_forwards(){
+  digitalWrite(motor_B_1, HIGH);
+  digitalWrite(motor_B_2, LOW);
+}
+
+void B_reverse(){
+  digitalWrite(motor_B_2, HIGH);
+  digitalWrite(motor_B_1, LOW);
+}
+
+void C_forwards(){
+  digitalWrite(motor_C_1, HIGH);
+  digitalWrite(motor_C_2, LOW);
+}
+
+void C_reverse(){
+  digitalWrite(motor_C_2, HIGH);
+  digitalWrite(motor_C_1, LOW);
+}
+
+
+void D_forwards(){
+  digitalWrite(motor_D_1, HIGH);
+  digitalWrite(motor_D_2, LOW);
+}
+
+void D_reverse(){
+  digitalWrite(motor_D_2, HIGH);
+  digitalWrite(motor_D_1, LOW);
+}
+
+
+  
+
