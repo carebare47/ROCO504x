9670dc5057313baede4a192591471ab22e53ea0b
carebare47 <tom_q@hotmail.com>
2017-11-02 15:22:59 +0000

    inital commit of kalman ball tracker, now also publishes a 1 or a 0 on the z axis of the point message to indicate whether a ball has been found or not

---
+++ kalman_ball_tracker/src/opencv_kalman2.cpp
@@ -0,0 +1,336 @@
+/******************************************
+ * OpenCV Tutorial: Ball Tracking using   *
+ * Kalman Filter                          *
+ ******************************************/
+#include "ros/ros.h"
+
+#include "geometry_msgs/Point.h"
+// Module "core"
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/core/core.hpp>
+
+// Module "highgui"
+#include <opencv2/highgui/highgui.hpp>
+
+// Module "imgproc"
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <opencv2/features2d/features2d.hpp>
+// Module "video"
+#include <opencv2/video/video.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <image_transport/image_transport.h>
+
+#include <sensor_msgs/image_encodings.h>
+// Output
+#include <iostream>
+#include <sstream>
+// Vector
+#include <vector>
+
+using namespace std;
+using namespace cv;
+// >>>>> Color to be tracked
+#define MIN_H_BLUE 200
+#define MAX_H_BLUE 300
+
+int low_h = 103,low_v = 6, low_s = 100,high_h = 118,high_v = 255, high_s = 255;
+// <<<<< Color to be tracked
+
+void on_low_r_thresh_trackbar(int, void *){return;}
+void on_high_r_thresh_trackbar(int, void *){return;}
+void on_low_g_thresh_trackbar(int, void *){return;}
+void on_high_g_thresh_trackbar(int, void *){return;}
+void on_low_b_thresh_trackbar(int, void *){return;}
+void on_high_b_thresh_trackbar(int, void *){return;}
+
+void createTrackbars(void){
+    /// Create Windows
+    namedWindow("HSV Bounds", 1);
+
+    createTrackbar("Low R","HSV Bounds", &low_h, 255, on_low_r_thresh_trackbar);
+    createTrackbar("High R","HSV Bounds", &high_h, 255, on_high_r_thresh_trackbar);
+    createTrackbar("Low G","HSV Bounds", &low_s, 255, on_low_g_thresh_trackbar);
+    createTrackbar("High G","HSV Bounds", &high_s, 255, on_high_g_thresh_trackbar);
+    createTrackbar("Low B","HSV Bounds", &low_v, 255, on_low_b_thresh_trackbar);
+    createTrackbar("High B","HSV Bounds", &high_v, 255, on_high_b_thresh_trackbar);
+
+
+
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "opencv_kalman2");
+    ros::NodeHandle n;
+    ros::Publisher Setpoint_Pub = n.advertise<geometry_msgs::Point>("coordinate_send_topic", 10);
+    ros::Rate loop_rate(10);
+    geometry_msgs::Point Setpoint;
+    // Camera frame
+    cv::Mat frame;
+
+    // >>>> Kalman Filter
+    int stateSize = 6;
+    int measSize = 4;
+    int contrSize = 0;
+
+    unsigned int type = CV_32F;
+    cv::KalmanFilter kf(stateSize, measSize, contrSize, type);
+    createTrackbars();
+    cv::Mat state(stateSize, 1, type);  // [x,y,v_x,v_y,w,h]
+    cv::Mat meas(measSize, 1, type);    // [z_x,z_y,z_w,z_h]
+    //cv::Mat procNoise(stateSize, 1, type)
+    // [E_x,E_y,E_v_x,E_v_y,E_w,E_h]
+
+    // Transition State Matrix A
+    // Note: set dT at each processing step!
+    // [ 1 0 dT 0  0 0 ]
+    // [ 0 1 0  dT 0 0 ]
+    // [ 0 0 1  0  0 0 ]
+    // [ 0 0 0  1  0 0 ]
+    // [ 0 0 0  0  1 0 ]
+    // [ 0 0 0  0  0 1 ]
+    cv::setIdentity(kf.transitionMatrix);
+
+    // Measure Matrix H
+    // [ 1 0 0 0 0 0 ]
+    // [ 0 1 0 0 0 0 ]
+    // [ 0 0 0 0 1 0 ]
+    // [ 0 0 0 0 0 1 ]
+    kf.measurementMatrix = cv::Mat::zeros(measSize, stateSize, type);
+    kf.measurementMatrix.at<float>(0) = 1.0f;
+    kf.measurementMatrix.at<float>(7) = 1.0f;
+    kf.measurementMatrix.at<float>(16) = 1.0f;
+    kf.measurementMatrix.at<float>(23) = 1.0f;
+
+    // Process Noise Covariance Matrix Q
+    // [ Ex   0   0     0     0    0  ]
+    // [ 0    Ey  0     0     0    0  ]
+    // [ 0    0   Ev_x  0     0    0  ]
+    // [ 0    0   0     Ev_y  0    0  ]
+    // [ 0    0   0     0     Ew   0  ]
+    // [ 0    0   0     0     0    Eh ]
+    //cv::setIdentity(kf.processNoiseCov, cv::Scalar(1e-2));
+    kf.processNoiseCov.at<float>(0) = 1e-2;
+    kf.processNoiseCov.at<float>(7) = 1e-2;
+    kf.processNoiseCov.at<float>(14) = 5.0f;
+    kf.processNoiseCov.at<float>(21) = 5.0f;
+    kf.processNoiseCov.at<float>(28) = 1e-2;
+    kf.processNoiseCov.at<float>(35) = 1e-2;
+
+    // Measures Noise Covariance Matrix R
+    cv::setIdentity(kf.measurementNoiseCov, cv::Scalar(1e-1));
+    // <<<< Kalman Filter
+
+    // Camera Index
+    int idx = 1;
+
+    // Camera Capture
+    cv::VideoCapture cap;
+
+    // >>>>> Camera Settings
+    if (!cap.open(idx))
+    {
+        cout << "Webcam not connected.\n" << "Please verify\n";
+        return EXIT_FAILURE;
+    }
+
+    cap.set(CV_CAP_PROP_FRAME_WIDTH, 320);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
+    // <<<<< Camera Settings
+
+    cout << "\nHit 'q' to exit...\n";
+
+    char ch = 0;
+
+    double ticks = 0;
+    bool found = false;
+
+    int notFoundCount = 0;
+
+    // >>>>> Main loop
+    while (ch != 'q' && ch != 'Q')
+    {
+        double precTick = ticks;
+        ticks = (double) cv::getTickCount();
+
+        double dT = (ticks - precTick) / cv::getTickFrequency(); //seconds
+
+        // Frame acquisition
+        cap >> frame;
+
+        cv::Mat res;
+        frame.copyTo( res );
+
+        if (found)
+        {
+            // >>>> Matrix A
+            kf.transitionMatrix.at<float>(2) = dT;
+            kf.transitionMatrix.at<float>(9) = dT;
+            // <<<< Matrix A
+
+            cout << "dT:" << endl << dT << endl;
+
+            state = kf.predict();
+            cout << "State post:" << endl << state << endl;
+
+            cv::Rect predRect;
+            predRect.width = state.at<float>(4);
+            predRect.height = state.at<float>(5);
+            predRect.x = state.at<float>(0) - predRect.width / 2;
+            predRect.y = state.at<float>(1) - predRect.height / 2;
+
+            cv::Point center;
+            center.x = state.at<float>(0);
+            center.y = state.at<float>(1);
+            cv::circle(res, center, 2, CV_RGB(255,0,0), -1);
+
+            cv::rectangle(res, predRect, CV_RGB(255,0,0), 2);
+        }
+
+        // >>>>> Noise smoothing
+        cv::Mat blur;
+        cv::GaussianBlur(frame, blur, cv::Size(5, 5), 3.0, 3.0);
+        // <<<<< Noise smoothing
+
+        // >>>>> HSV conversion
+        cv::Mat frmHsv;
+        cv::cvtColor(blur, frmHsv, CV_BGR2HSV);
+        // <<<<< HSV conversion
+
+        // >>>>> Color Thresholding
+        // Note: change parameters for different colors
+        cv::Mat rangeRes = cv::Mat::zeros(frame.size(), CV_8UC1);
+        cv::inRange(frmHsv, cv::Scalar(low_h, low_s, low_v),
+                    cv::Scalar(high_h, high_s, high_v), rangeRes);
+        // <<<<< Color Thresholding
+
+        // >>>>> Improving the result
+        cv::erode(rangeRes, rangeRes, cv::Mat(), cv::Point(-1, -1), 2);
+        cv::dilate(rangeRes, rangeRes, cv::Mat(), cv::Point(-1, -1), 2);
+        // <<<<< Improving the result
+
+        // Thresholding viewing
+        cv::imshow("Threshold", rangeRes);
+
+        // >>>>> Contours detection
+        vector<vector<cv::Point> > contours;
+        cv::findContours(rangeRes, contours, CV_RETR_EXTERNAL,
+                         CV_CHAIN_APPROX_NONE);
+        // <<<<< Contours detection
+
+        // >>>>> Filtering
+        vector<vector<cv::Point> > balls;
+        vector<cv::Rect> ballsBox;
+        for (size_t i = 0; i < contours.size(); i++)
+        {
+            cv::Rect bBox;
+            bBox = cv::boundingRect(contours[i]);
+
+            float ratio = (float) bBox.width / (float) bBox.height;
+            if (ratio > 1.0f)
+                ratio = 1.0f / ratio;
+
+            // Searching for a bBox almost square
+            if (ratio > 0.75 && bBox.area() >= 4000)
+            {
+                balls.push_back(contours[i]);
+                ballsBox.push_back(bBox);
+            }
+        }
+        // <<<<< Filtering
+
+        cout << "Balls found:" << ballsBox.size() << endl;
+//if (balls.size()>=2){
+        // >>>>> Detection result
+
+        for (size_t i = 0; i < balls.size(); i++)
+        {
+            cv::drawContours(res, balls, i, CV_RGB(20,150,20), 1);
+            cv::rectangle(res, ballsBox[i], CV_RGB(0,255,0), 2);
+
+            cv::Point center;
+            center.x = ballsBox[i].x + ballsBox[i].width / 2;
+            center.y = ballsBox[i].y + ballsBox[i].height / 2;
+            cv::circle(res, center, 2, CV_RGB(20,150,20), -1);
+            Setpoint.x = center.x;
+            Setpoint.y = center.y;
+            Setpoint.z = 0;
+            Setpoint_Pub.publish(Setpoint);
+            stringstream sstr;
+            sstr << "(" << center.x << "," << center.y << ")";
+            cv::putText(res, sstr.str(),
+                        cv::Point(center.x + 3, center.y - 3),
+                        cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(20,150,20), 2);
+        }
+        // <<<<< Detection result
+//}
+        // >>>>> Kalman Update
+        if (balls.size() == 0)
+        {
+            notFoundCount++;
+            cout << "notFoundCount:" << notFoundCount << endl;
+            Setpoint.x = 0;
+            Setpoint.y = 0;
+            Setpoint.z = 1;
+            Setpoint_Pub.publish(Setpoint);
+            if( notFoundCount >= 100 )
+            {
+                found = false;
+            }
+            /*else
+                kf.statePost = state;*/
+        }
+        else
+        {
+            notFoundCount = 0;
+
+            meas.at<float>(0) = ballsBox[0].x + ballsBox[0].width / 2;
+            meas.at<float>(1) = ballsBox[0].y + ballsBox[0].height / 2;
+            meas.at<float>(2) = (float)ballsBox[0].width;
+            meas.at<float>(3) = (float)ballsBox[0].height;
+
+            if (!found) // First detection!
+            {
+                // >>>> Initialization
+                kf.errorCovPre.at<float>(0) = 1; // px
+                kf.errorCovPre.at<float>(7) = 1; // px
+                kf.errorCovPre.at<float>(14) = 1;
+                kf.errorCovPre.at<float>(21) = 1;
+                kf.errorCovPre.at<float>(28) = 1; // px
+                kf.errorCovPre.at<float>(35) = 1; // px
+
+                state.at<float>(0) = meas.at<float>(0);
+                state.at<float>(1) = meas.at<float>(1);
+                state.at<float>(2) = 0;
+                state.at<float>(3) = 0;
+                state.at<float>(4) = meas.at<float>(2);
+                state.at<float>(5) = meas.at<float>(3);
+                // <<<< Initialization
+
+                kf.statePost = state;
+                
+                found = true;
+            }
+            else
+                kf.correct(meas); // Kalman Correction
+
+            cout << "Measure matrix:" << endl << meas << endl;
+        }
+        // <<<<< Kalman Update
+
+        // Final result
+        cv::imshow("Tracking", res);
+        ros::spinOnce();
+
+        loop_rate.sleep();
+        // User key
+        ch = cv::waitKey(1);
+
+    }
+    // <<<<< Main loop
+
+    return EXIT_SUCCESS;
+}
