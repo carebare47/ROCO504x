7c4d7e902278ebc1a3667ef32094d7dc068ba9ae
carebare47 <tom_q@hotmail.com>
2017-12-14 12:14:11 +0000

    throw code uppdated

--- throw_controller/Roco504ThrowControl/Roco504ThrowControl.ino
+++ throw_controller/Roco504ThrowControl/Roco504ThrowControl.ino
@@ -11,25 +11,32 @@ void spool_cb(const std_msgs::Int16 &spoolControl) {
   spoolCommand = spoolControl.data;
 }
 
+ros::NodeHandle nh;
 ros::Subscriber<std_msgs::Int16> ThrowSpool("throw_motor", &spool_cb);
 void setup() {
+    nh.initNode();
+  //Initialize subscribers & publishers
+  nh.subscribe(ThrowSpool);
+  
 }
 
 void loop() {
 
   if (spoolCommand == 0) {
     spoolMotorCommand = 128;
-    analogWrite(motorControlPin, spoolMotorCommand);
+    analogWrite(MotorControlPin, spoolMotorCommand);
   }
 
   else if (spoolCommand == 1) {
     spoolMotorCommand = 0;
-    analogWrite(motorControlPin, spoolMotorCommand);
+    analogWrite(MotorControlPin, spoolMotorCommand);
   }
 
   else if (spoolCommand == 2) {
     spoolMotorCommand = 255;
-    analogWrite(motorControlPin, spoolMotorCommand);
+    analogWrite(MotorControlPin, spoolMotorCommand);
   }
+
+  nh.spinOnce();
 
 }
--- throw_controller/src/throw_controller_manual.cpp
+++ throw_controller/src/throw_controller_manual.cpp
@@ -5,8 +5,12 @@
 int throwControl = -1;
 const float clampClose = 4.5;
 const float clampOpen = 2.5;
+bool step = false;
 void FrameClamps_cb(const std_msgs::Int16::ConstPtr& clamps_command){
   throwControl = clamps_command->data;
+}
+void step_pub_cb(const std_msgs::Bool::ConstPtr& booleybooley){
+	step = booleybooley->data;
 }
 
 int main(int argc, char **argv){
@@ -23,6 +27,7 @@ int main(int argc, char **argv){
 	ros::Publisher ThrowSpoolPub = n.advertise<std_msgs::Int16>("/throw_motor", 1);
 
   	ros::Subscriber FrameClampsSub = n.subscribe("/frame_clamps_command", 1, FrameClamps_cb);
+  	ros::Subscriber step_pub = n.subscribe("/throw_steps", 1, step_pub_cb);
 
   	std_msgs::Float64 cFrameClamps;
   	std_msgs::Float64 cThrowClamp;
@@ -34,10 +39,14 @@ int main(int argc, char **argv){
   	while (ros::ok()){
     	if (throwControl == 1){
 
+    		//close frame clamps
     		cFrameClamps.data = clampClose;
+    		//start winder winding
     		cThrowSpool.data = Wind;
+    		//close throw clamp
     		cThrowClamp.data = 2;
 
+    		//publish frame clamps closed
     		Clamp1Pub.publish(cFrameClamps);
     		Clamp2Pub.publish(cFrameClamps);
     		Clamp3Pub.publish(cFrameClamps);
@@ -45,23 +54,36 @@ int main(int argc, char **argv){
     		Clamp4Pub.publish(cFrameClamps);
     		sleep(2);
 
+    		////begin winding
     		ThrowSpoolPub.publish(cThrowSpool);
-    		sleep(spoolTimeWind);
+    		//wait until wound
+    		while (!step){
 
+    		}
+    		step = false;
+    		//sleep(spoolTimeWind);
+
+    		//stop winding
     		cThrowSpool.data = 0;
     		ThrowSpoolPub.publish(cThrowSpool);
+    		//shut throw clamp
     		ThrowClampPub.publish(cThrowClamp);
     		sleep(2);
 
+    		//begin unwinding
     		cThrowSpool.data = unWind;
     		ThrowSpoolPub.publish(cThrowSpool);
     		sleep(spoolTimeUnwind);
+    		//stop unwinding
     		cThrowSpool.data = 0;
     		ThrowSpoolPub.publish(cThrowSpool);
     		
+    		//open throw clamp (throw!)
     		cThrowClamp.data = 4;
     		ThrowClampPub.publish(cThrowClamp);
     		sleep(5);
+
+    		//open frame clamps
     		cFrameClamps.data = clampOpen;
     		Clamp1Pub.publish(cFrameClamps);
     		Clamp2Pub.publish(cFrameClamps);
